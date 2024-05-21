02866b5cbbcb4663243806750af7ff9f6dce8710
joe_tian_forexco_home_macmini <j.tian@forexco.com.au>
2024-05-22 00:43:18 +1000

    fix:updates

--- infra/lib/file-upload-stack.ts
+++ infra/lib/file-upload-stack.ts
@@ -7,8 +7,11 @@ export class FileUploadStack extends Stack {
     super(scope, id, props);
 
     // Create an S3 bucket
+    console.log("Creating S3 bucket", process.env.AWS_S3_BUCKET_NAME);
     const clientBucket = new s3.Bucket(this, "PTX-FileUploadBucket", {
-      bucketName: "ptx-file-upload-bucket-test",
+      bucketName: 'ptx-file-upload-bucket-test-one',
+      versioned: true,
+      publicReadAccess: false,
     });
 
     console.log("Creating S3 bucket", clientBucket);
